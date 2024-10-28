7ae33506274d7e498ffda2877f248eaa8e15f590
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-10-27 22:38:18 -0500

    Added JWT support in swagger

---
+++ SPSAPI/Controllers/SparePartController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SPSAPI.Data;
+using SPSModels.Models;
+
+namespace SPSAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize(Roles = "Client")]
+	public class SparePartController : ControllerBase
+	{
+		private readonly ApplicationDBContext _context;
+
+		public SparePartController(ApplicationDBContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		[Route("/all")]
+		public ActionResult<List<SparePart>> GetAll()
+		{
+			return Ok(_context.SparePart.ToList());
+		}
+	}
+}
--- SPSAPI/Program.cs
+++ SPSAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SPSAPI.Data;
 using SPSAPI.Utilities;
 using System.Text;
@@ -21,9 +22,11 @@ builder.Services.AddAuthentication(options =>
 {
 	options.TokenValidationParameters = new()
 	{
-		ValidateIssuer = false,
-		ValidateAudience = false,
+		ValidateIssuer = true,
+		ValidateAudience = true,
 		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["JWTSettings:Issuer"]!,
+		ValidAudience = builder.Configuration["JWTSettings:Audience"]!,
 		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:Key"]!))
 	};
 });
@@ -32,9 +35,38 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddScoped<IJWTTokenGenerator, JWTTokenGenerator>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configure Swagger for use with JWT. Code obtained from https://medium.com/@deidra108/oauth-bearer-token-with-swagger-ui-net-6-0-86835e616deb and modified.
+builder.Services.AddSwaggerGen(options =>
+{
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Description = "Enter token",
+		Name = "Authorization",
+		Type = SecuritySchemeType.Http,
+		BearerFormat = "JWT",
+		Scheme = "bearer"
+	});
+
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			Array.Empty<string>()
+		}
+	});
+});
 
 var app = builder.Build();
 
