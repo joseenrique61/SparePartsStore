8695bad776d8ec1d910c170d586caced8f92f6a4
Ariel Mateo Anchapaxi Moreta <ariel.anchapaxi@udla.edu.ec>
2024-11-01 10:48:17 -0500

    Changes for the view of SparePart

--- SparePartsStore/Controllers/SparePartController.cs
+++ SparePartsStore/Controllers/SparePartController.cs
@@ -2,40 +2,131 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SparePartsStoreWeb.Data.UnitOfWork;
+using SPSModels.Models;
 
 namespace SparePartsStoreWeb.Controllers
 {
     public class SparePartController : Controller
     {
-        private readonly UnitOfWork _unitOfWork;
-        public SparePartController(UnitOfWork unitOfWork) { 
+        private readonly IUnitOfWork _unitOfWork;
+        public SparePartController(IUnitOfWork unitOfWork) { 
             _unitOfWork = unitOfWork;
         }
 
-        // GET: SparePart
-        public async Task<IActionResult> Index() {
-            //return View(await _unitOfWork.SparePart.ToListAsync());
-            return View();
+        public async Task<IActionResult> Index() 
+        {
+            return View(await _unitOfWork.SparePart.GetAll());
         }
 
-        // GET: Estadios/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int id)
         {
-            if (id == null)
+            var SparePart = await _unitOfWork.SparePart
+                 .GetById(id);
+            if (SparePart == null)
             {
                 return NotFound();
             }
 
-            return View();
-            /** var SparePart = await _unitOfWork.SparePart
-                 .FirstOrDefaultAsync(m => m.Id == id);
-             if (SparePart == null)
-             {
-                 return NotFound();
-             }
-
-             return View(SparePart);**/
+            return View(SparePart);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Stock,Image,CategoryId")] SparePart sparePart)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.SparePart.Create(sparePart);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sparePart);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var estadio = await _unitOfWork.SparePart.GetById(id);
+            if (estadio == null)
+            {
+                return NotFound();
+            }
+            return View(estadio);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Stock,Image,CategoryId")] SparePart sparePart)
+        {
+            if (id != sparePart.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _unitOfWork.SparePart.Update(sparePart);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SparePartExist(sparePart.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sparePart);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            /*
+             if (id == null)
+            {
+                return NotFound();
+            }
+            */
+
+            var SparePart = await _unitOfWork.SparePart
+                 .GetById(id);
+            if (SparePart == null)
+            {
+                return NotFound();
+            }
+
+            return View(SparePart);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var estadio = await _unitOfWork.SparePart.GetById(id);
+            if (estadio != null)
+            {
+                await _unitOfWork.SparePart.Delete(id);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SparePartExist(int id)
+        {
+            if (_unitOfWork.SparePart.GetById(id) != null)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
