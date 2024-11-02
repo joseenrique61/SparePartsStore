859d26a9d20a025af6b33c986344c870651c94af
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2024-11-01 22:12:08 -0500

    Shopping cart view/controller created

---
+++ SparePartsStore/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SparePartsStoreWeb.Data.UnitOfWork;
+
+namespace SparePartsStoreWeb.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PurchaseOrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> CartInfo()
+        {
+            return View();
+        }
+    }
+}
