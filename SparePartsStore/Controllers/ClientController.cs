499b89f7e93546db481ce80e00787614d0008c50
Ariel Mateo Anchapaxi Moreta <ariel.anchapaxi@udla.edu.ec>
2024-11-02 19:12:48 -0500

    Changes in the ClientController.cs and IClientRepository.cs

--- SparePartsStore/Controllers/ClientController.cs
+++ SparePartsStore/Controllers/ClientController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SparePartsStoreWeb.Data.UnitOfWork;
+using SPSModels.Models;
 
 namespace SparePartsStoreWeb.Controllers
 {
@@ -13,14 +14,32 @@ namespace SparePartsStoreWeb.Controllers
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<IActionResult> Login()
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetInt32("ClientId") != null && HttpContext.Session.GetString("Email") != null && HttpContext.Session.GetString("Role") != null) {
+                return View(_unitOfWork.Client.Login(HttpContext.Session.GetString("Email"), HttpContext.Session.GetString("Role")));
+            }
+
+            return View();
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
 
-        public async Task<IActionResult> Create()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,Name,Address,City,Country")] Client client)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (await _unitOfWork.Client.Register(client) == true)
+                {
+                    return RedirectToAction(nameof(Login));
+                } 
+            }
+            return View(client);
         }
     }
 }
--- SparePartsStore/Data/Repositories/ClientRepository/IClientRepository.cs
+++ SparePartsStore/Data/Repositories/ClientRepository/IClientRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace SparePartsStoreWeb.Data.Repositories.ClientRepository
+﻿using SPSModels.Models;
+
+namespace SparePartsStoreWeb.Data.Repositories.ClientRepository
 {
     public interface IClientRepository
     {
         public Task<bool> Login(string email, string password);
+        public Task<bool> Register(Client client);
     }
 }
