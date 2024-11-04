36c31ae11f718bbca1b702753304d3501e09f94f
Ariel Mateo Anchapaxi Moreta <ariel.anchapaxi@udla.edu.ec>
2024-11-03 21:21:59 -0500

    Changes in the view of details.

---
+++ SparePartsStore/Controllers/BuyController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SparePartsStoreWeb.Data.UnitOfWork;
+using SPSModels.Models;
+
+namespace SparePartsStoreWeb.Controllers
+{
+    public class BuyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BuyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Buy(int id)
+        {
+            var sparePart = await _unitOfWork.SparePart
+                             .GetById(id);
+
+            if (sparePart == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Id = sparePart.Id;
+            ViewBag.Name = sparePart.Name;
+            ViewBag.Description = sparePart.Description;
+            ViewBag.Stock = sparePart.Stock;
+            ViewBag.Category = sparePart?.Category.Name;
+            ViewBag.Price = sparePart.Price;
+
+
+            return View(new PurchaseOrder());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Buy([Bind("Id,PurchaseOrderId,SparePartId,Ammount")] PurchaseOrder purchaseOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.PurchaseOrder.Create(purchaseOrder);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(purchaseOrder);
+        }
+
+    }
+}
