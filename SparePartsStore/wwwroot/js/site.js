ca098f52abcffe68a6c0850a8f1358680f395ebc
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-11-04 09:46:07 -0500

    Fixed error with - y + buttons in details view

--- SparePartsStore/wwwroot/js/site.js
+++ SparePartsStore/wwwroot/js/site.js
@@ -25,3 +25,33 @@ function AddToCart() {
 
     addForm.setAttribute("action", addForm.getAttribute("action") + "?amount=" + amount);
 }
+
+function IncreaseAmount(max) {
+    const increaseButton = document.getElementById("increaseButton")
+    const decreaseButton = document.getElementById("decreaseButton")
+    let amount = parseInt(document.getElementById("amount").innerText)
+
+    amount += 1
+
+    decreaseButton.classList.remove("disabled")
+    if (amount >= max) {
+        increaseButton.classList.add("disabled")
+    }
+
+    document.getElementById("amount").innerText = amount
+}
+
+function DecreaseAmount() {
+    const increaseButton = document.getElementById("increaseButton")
+    const decreaseButton = document.getElementById("decreaseButton")
+    let amount = parseInt(document.getElementById("amount").innerText)
+
+    amount -= 1
+
+    increaseButton.classList.remove("disabled")
+    if (amount <= 0) {
+        decreaseButton.classList.add("disabled")
+    }
+
+    document.getElementById("amount").innerText = amount
+}
