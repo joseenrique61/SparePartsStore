8e744861b528bebb3dd9b2794aa5f1a825750e88
Ariel Mateo Anchapaxi Moreta <ariel.anchapaxi@udla.edu.ec>
2024-11-03 22:04:52 -0500

    It's still alive!

---
+++ SparePartsStore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SparePartsStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
