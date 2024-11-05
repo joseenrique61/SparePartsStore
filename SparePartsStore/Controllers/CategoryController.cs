a11d94343e1612ce5b360032f66bb5198ac35f66
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-11-04 20:38:05 -0500

    Edited views for categories

--- SparePartsStore/Controllers/CategoryController.cs
+++ SparePartsStore/Controllers/CategoryController.cs
@@ -1,126 +1,98 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using SparePartsStoreWeb.Data.UnitOfWork;
 using SPSModels.Models;
 
 namespace SparePartsStoreWeb.Controllers
 {
-    public class CategoryController : BaseController
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        public CategoryController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
+	public class CategoryController : BaseController
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public CategoryController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            return View(await _unitOfWork.Category.GetAll());
-        }
+		public async Task<IActionResult> Index()
+		{
+			return View(await _unitOfWork.Category.GetAll());
+		}
 
-        public async Task<IActionResult> Details(int id)
-        {
-            var category = await _unitOfWork.Category
-                 .GetById(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
+		public async Task<IActionResult> Details(int id)
+		{
+			var category = await _unitOfWork.Category
+				 .GetById(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
 
-            return View(category);
-        }
+			return View(category);
+		}
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                await _unitOfWork.Category.Create(category);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				await _unitOfWork.Category.Create(category);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(category);
+		}
 
-        public async Task<IActionResult> Edit(int id)
-        {
-            var category = await _unitOfWork.Category.GetById(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
+		public async Task<IActionResult> Edit(int id)
+		{
+			var category = await _unitOfWork.Category.GetById(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
-        {
-            if (id != category.Id)
-            {
-                return NotFound();
-            }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
+		{
+			if (id != category.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    await _unitOfWork.Category.Update(category);
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExist(category.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
+			if (ModelState.IsValid)
+			{
+				await _unitOfWork.Category.Update(category);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(category);
+		}
 
-        public async Task<IActionResult> Delete(int id)
-        {
-            var category = await _unitOfWork.Category
-                 .GetById(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var category = await _unitOfWork.Category.GetById(id);
+			if (category != null)
+			{
+				await _unitOfWork.Category.Delete(id);
+			}
 
-            return View(category);
-        }
+			return RedirectToAction(nameof(Index));
+		}
 
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var category = await _unitOfWork.Category.GetById(id);
-            if (category != null)
-            {
-                await _unitOfWork.Category.Delete(id);
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryExist(int id)
-        {
-            if (_unitOfWork.Category.GetById(id) != null)
-            {
-                return true;
-            }
-            return false;
-        }
-    }
+		private bool CategoryExist(int id)
+		{
+			if (_unitOfWork.Category.GetById(id) != null)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
 }
