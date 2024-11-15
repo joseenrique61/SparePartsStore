ced8aa7df32a6f4528d1cff4a99b69940d6c7fe1
José Enrique Cerezo <joseenrique61@hotmail.com>
2024-11-15 11:57:59 -0500

    Created the maui project

---
+++ SPSMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SPSMobile
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ SPSMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SPSMobile
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
